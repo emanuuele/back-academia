58e1088dd742f5b3d912ab5dbaaf0ea0693c818b
Emanuele <85975763+emanuuele@users.noreply.github.com>
2024-11-07 20:15:51 -0300

    Update client.js

--- routes/client.js
+++ routes/client.js
@@ -8,7 +8,7 @@ router.get("/", (req, res) => {
   execSQLQuery(`SELECT id, nome, altura, peso, idade, DATE_FORMAT(STR_TO_DATE(nascimento, '%d/%m/%Y'), '%d-%m-%Y') as nascimento, DATE_FORMAT(STR_TO_DATE(ultimoPagamento, '%Y-%m-%d'), '%d-%m-%Y') as ultimoPagamento FROM Clients where ativo is null or ativo = 'S'`, res);
 });
 
-router.post("/clients", (req, res) => {
+router.post("/", (req, res) => {
   const {
     clientData: { nome, idade, peso, altura, nascimento, ultimoPagamento },
   } = req.body;
@@ -17,12 +17,12 @@ router.post("/clients", (req, res) => {
     res
   );
 });
-router.get('/clients/:param?', (req,res)=>{
+router.get('/:param?', (req,res)=>{
   let param = req.params.param
   execSQLQuery(`SELECT * FROM Clients where nome like '%${param}%' and ativo = 'S' or id like '%${param}%' and ativo = 'S'`, res)
 })
 
-router.put("/clients/:id?", (req, res) => {
+router.put("/:id?", (req, res) => {
   const id = parseInt(req.params.id);
   const {
     clientData: { nome, idade, peso, altura, nascimento },
@@ -36,13 +36,13 @@ router.put("/clients/:id?", (req, res) => {
   );
 });
 
-router.delete("/clients/:id?", (req, res) => {
+router.delete("/:id?", (req, res) => {
   const id = parseInt(req.params.id);
   const flag = "N";
   execSQLQuery(`UPDATE Clients SET ativo="${flag}" WHERE ID = ${id}`, res);
 });
 
-router.get("/clients/:id?", (req, res, next) => {
+router.get("/:id?", (req, res, next) => {
   let filter = "";
   if (req.params.id) filter = " WHERE ID= " + parseInt(req.params.id);
   execSQLQuery("SELECT * FROM Clients" + filter, res);
