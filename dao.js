430f3bdd6d7fcad3d55f5cb3b4782e16088a9253
Emanuele <85975763+emanuuele@users.noreply.github.com>
2024-11-08 10:28:54 -0300

    Update dao.js

--- dao.js
+++ dao.js
@@ -1,34 +1,26 @@
-const {
-  createTableClients,
-  createTablePagamentos,
-} = require("./create-table.js");
 const mysql = require("mysql2");
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: "db4free.net",
   port: 3306,
   user: "demisroot",
   password: "javajava",
   database: "academiaestagiog",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
   multipleStatements: true,
-});
-connection.connect((err) => {
-  if (err) return console.log(err);
-  //createTableClients(connection);
-  //createTablePagamentos(connection);
 });
 
 function execSQLQuery(sqlQry, res) {
-  connection.query(sqlQry, (error, result, fields) => {
+  pool.query(sqlQry, (error, result, fields) => {
     if (error) {
       console.log({ euSouUmErro: error });
       res.json(error);
     } else {
       res?.json(result);
     }
-
-    //connection.end();
     console.log("executou");
   });
 }
 
-module.exports = execSQLQuery
+module.exports = execSQLQuery;
